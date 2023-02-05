7ed53251e1ffb4434a3aeab7d6ffa19d998361f6
Tejas KS <tejas.ks.007@gmail.com>
2023-02-05 21:40:59 +0530

    creating ec2 resources

--- ec2/main.tf
+++ ec2/main.tf
@@ -26,27 +26,12 @@ resource "aws_security_group" "webserver_access" {
 
 
 }
-#security group end here
 
-#create block storage
-resource "aws_ebs_volume" "data_vol" {
-        availability_zone = "ap-south-1a"
-        size = 5
-}
-
-resource "aws_volume_attachment" "ourfirst-vol" {
-        device_name = "/dev/sdc"
-        volume_id = "${aws_ebs_volume.data_vol.id}"
-        instance_id = "${aws_instance.ourfirst.id}"
-}
-
-#block storage end here
 resource "aws_instance" "ourfirst" {
   ami           = "ami-01a4f99c4ac11b03c"
   availability_zone = "ap-south-1a"
   instance_type = "t2.micro"
   security_groups = ["${aws_security_group.webserver_access.name}"]
-  user_data = filebase64("install_httpd.sh")
   key_name = "zoomkey"
   tags = {
     Name  = "ec2-test"
